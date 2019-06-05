aea8cb255d5ba4ba79e86fc5d06164f02bd8ad2a
milespucarelli <miles.pucarelli@gmail.com>
2019-06-04 22:57:07 -0400

    home page component start

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 class Home extends Component {
 
   render() {
     return (
       <div id='background'>
+        <Container text>
+        <Header as='h1' content='TABULATOR' inverted />
+        </Container>
       </div>
     );
   }
