779c1b20704e5c91a97b35efb4df233a7eb1d60e
milespucarelli <miles.pucarelli@gmail.com>
2019-06-18 03:05:00 -0400

    Semantic UI for login and signup forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Button, Form } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 
 const Login = (props) => {
   const {email, password, error} = props
@@ -7,7 +9,7 @@ const Login = (props) => {
     <div id='login-card'>
       <div id='login-inner-card'>
         <h1 className='header'>Login</h1>
-        <form onSubmit={props.submitHandler}>
+        { /*<form onSubmit={props.submitHandler}>
           <input
             className={error === 'Invalid username or password' ? 'invalid' : ''}
             id='email-input'
@@ -26,7 +28,30 @@ const Login = (props) => {
             onChange={props.changeHandler}/><br/>
           {error === 'Invalid username or password' && <p className='error-text'>{error}</p>}
           <input type='submit' />
-        </form>
+        </form> */ }
+        <Form onSubmit={props.submitHandler}>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='email'
+              name='email'
+              value={email}
+              placeholder='Email...'
+              onChange={props.changeHandler}
+              error={error === 'Invalid username or password' ? true : undefined}/>
+          </Form.Field>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='password'
+              name='password'
+              value={password}
+              placeholder='Password...'
+              onChange={props.changeHandler}
+              error={error === 'Invalid username or password' ? true : undefined}/>
+          </Form.Field>
+          <Button id='submit-button' type='submit'>Submit</Button>
+        </Form>
         <p className='link-caption'>New to Tabulator?</p>
         <p id='link' onClick={props.clickHandler}>Sign Up!</p>
       </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, Form } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 
 const SignUp = (props) => {
   const {
@@ -16,7 +18,7 @@ const SignUp = (props) => {
     <div id='signup-card'>
       <div id='signup-inner-card'>
         <h1 className='header'>Sign Up</h1>
-        <form onSubmit={submitHandler}>
+        {/* <form onSubmit={submitHandler}>
           <input
             id='username-input'
             type='text'
@@ -54,9 +56,58 @@ const SignUp = (props) => {
             rows='5'
             onChange={changeHandler}></textarea>
           <input type='submit' />
-          <p className='link-caption'>Already a user?</p>
-          <p id='link' onClick={clickHandler}>Log In!</p>
-        </form>
+        </form> */}
+        <Form onSubmit={submitHandler}>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='text'
+              name='username'
+              value={username}
+              placeholder='Username...'
+              onChange={changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='email'
+              name='email'
+              value={email}
+              placeholder='Email...'
+              onChange={changeHandler} />
+          </Form.Field>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='password'
+              name='password'
+              value={password}
+              placeholder='Password...'
+              onChange={changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <input
+              className='form-input'
+              type='text'
+              name='image'
+              value={image}
+              placeholder='Image Link...'
+              onChange={changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <textarea
+              className='form-input'
+              type='text'
+              name='bio'
+              value={bio}
+              placeholder='Bio...'
+              rows='5'
+              onChange={changeHandler}></textarea>
+          </Form.Field>
+          <Button id='submit-button' type='submit'>Submit</Button>
+        </Form>
+        <p className='link-caption'>Already a user?</p>
+        <p id='link' onClick={clickHandler}>Log In!</p>
       </div>
     </div>
   )
